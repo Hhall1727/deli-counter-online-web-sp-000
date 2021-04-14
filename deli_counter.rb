a19b829c4728139064bee4f966c69e2b4fd1ee70
Halie Hall <halierae17@outlook.com>
2021-04-14 17:12:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  line(katz_deli) << name
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
