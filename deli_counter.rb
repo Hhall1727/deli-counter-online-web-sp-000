f264c1e1d755630d59190c49771451da188a7eb5
Halie Hall <halierae17@outlook.com>
2021-04-14 17:07:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else katz_deli.size > 0 
+    katz_deli = []
+    katz_deli.each do |num, name|
+    puts "The line is currently: #{num + 1} #{name}."
+   end
+   katz_deli
+  end
+end
+
+def take_a_number(katz_deli, name)
+  line(katz_deli) << name
+end
+
+def now_serving(katz_deli)
+  
+end
